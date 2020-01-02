d9e6d3be2d640d052506c21bc4b9f7edb44327ca
SHP92 <sunliebsh@naver.com>
2020-01-02 23:57:24 +0900

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,93 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet
+  , Text
+  , View
+  , StatusBar
+  , TextInput
+  , Dimensions
+  , Platform, 
+  ScrollView} from 'react-native';
+  import Todo from './Todo.js';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+const { width, height } = Dimensions.get('window');
+
+class App extends React.Component {
+  state = {
+    input: ''
+  };
+
+  render() {
+    const { input } = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle='light-content' />
+        <Text style={styles.title}> TODO list </Text>
+        <View style={styles.card}>
+          <TextInput 
+            style={styles.input} 
+            placeholder='new to do'
+            value={input}
+            onChangeText={this.getNewInput}
+          />
+          <ScrollView style={styles.scroll}>
+            <Todo />
+          </ScrollView>
+        </View>
     </View>
-  );
+    )
+  }
+
+  // getNewInput(text)로 사용할 경우 onChageText event에 따로 bind 해줘야함
+  getNewInput = text => {
+    this.setState({ input: text });
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F23657',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
+  , title: {
+    color: 'white'
+    , fontSize: 40
+    , marginTop: 40
+    , fontWeight: "300"
+  }
+  , card: {
+    flex: 1
+    , backgroundColor: 'white'
+    , width: width - 50
+    , marginTop: 20
+    , borderTopLeftRadius: 15
+    , borderTopRightRadius: 15
+    // SHADOW : platform customizing code
+    , ...Platform.select({
+      ios: {
+        shadowColor: 'rgb(50, 50, 50)'
+        , shadowOpacity: 0.5
+        , shadowRadius: 0.5
+        , shadowOffset: {
+          height: -2
+          , width: 2
+        }
+      }
+      , android: {
+        elevation: 3
+      }
+    })
+  }
+  , input: {
+    fontSize: 20
+    , padding: 20
+    , borderBottomColor: 'lightgrey'
+    , borderBottomWidth: 1
+  }
+  , scroll: {
+
+  }
 });
+
+export default App;
---
+++ Todo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text, StyleSheet, View, TouchableOpacity } from 'react-native';
+
+class Todo extends React.Component {
+    state = {
+
+    }
+    render() {
+        return (
+            <Text> text </Text>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default Todo;
